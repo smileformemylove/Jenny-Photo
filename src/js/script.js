dd21bd08c8bf60b14b71760283c5b09c03cf1db8
Mykola <Shekezzz@gmail.com>
2024-11-03 20:18:50 +0200

    my site v0.4

--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,13 @@
 'use strict';
 
+// const albums = document.querySelectorAll('.album');
+//
+// albums.forEach(album => {
+//     album.addEventListener('mouseenter', () => {
+//         album.querySelector('.photo-grid').style.display = 'grid';
+//     });
+//
+//     album.addEventListener('mouseleave', () => {
+//         album.querySelector('.photo-grid').style.display = 'none';
+//     });
+// });
